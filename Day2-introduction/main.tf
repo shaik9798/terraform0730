00cb4f20faa0f97a6c8ebb82b3b7e1d08f2f535f
shaik9798 <shaikriyaz1271@gmail.com>
2025-07-26 10:47:32 +0530

    Update main.tf

--- Day2-introduction/main.tf
+++ Day2-introduction/main.tf
@@ -1,15 +1,7 @@
 resource "aws_instance" "name" {
-  ami = "ami-05ffe3c48a9991133"
+  ami = "ami-054b7fc3c333ac6d2"
   instance_type = "t2.micro"
   tags = {
- Name = dev-1
-  }
-}
-
-
-resource "aws_vpc" "name"{
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = dev-1
+ Name = "dev-1"
   }
 }
