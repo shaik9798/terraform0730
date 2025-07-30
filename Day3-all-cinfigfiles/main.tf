141a3f3c55298fe4ddfa1632e84953a2dbfdbc61
shaik9798 <shaikriyaz1271@gmail.com>
2025-07-30 11:44:13 +0530

    Update main.tf

--- Day3-all-cinfigfiles/main.tf
+++ Day3-all-cinfigfiles/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "name" {
   ami = var.ami_id
   instance_type = var.instance_type
   tags = {
-    Name = "day-3"
+    Name = "test"
   }
 
 }
@@ -10,7 +10,7 @@ resource "aws_instance" "day-4" {
   ami = var.ami_id
   instance_type = var.instance_type
   tags = {
-    Name = "day-5"
+    Name = "dev"
   }
 
 }
